6b4075e710aa305166bb4c9715d9feae67f2f308
Liam York <yorkliam08@gmail.com>
2025-04-11 20:10:08 -0500

    script for changing the color

---
+++ Assets/Scripts/DimensionsScript.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject floor;
+
+    public float currentTime;
+
+    public SpriteRenderer pallette;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.Instance.swapped == true)
+        {
+            pallette.color = new Color(0f, 0f, 0f, 1f);
+            Debug.Log("Fart");
+        }
+        else
+        {
+            pallette.color = new Color(1f, 1f, 1f, 1f);
+        }
+
+        if(GameManager.Instance.isSwapped == true)
+        {
+            currentTime  += Time.deltaTime;
+        }
+        if(currentTime >= 5.0f)
+        {
+            GameManager.Instance.swapped = false;
+            GameManager.Instance.isSwapped = false;
+            currentTime = 0.0f;
+        }
+    }
+}
