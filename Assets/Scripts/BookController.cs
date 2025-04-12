99ae799ee5d265292c492fafd164c431c11f0b22
Lily <ndd232@gmail.com>
2025-04-12 10:27:17 -0500

    Fixed shooting bug

--- Assets/Scripts/BookController.cs
+++ Assets/Scripts/BookController.cs
@@ -7,44 +7,73 @@ using UnityEngine.InputSystem;
 public class BookController : MonoBehaviour
 {
     public float dead_zone;
+<<<<<<< Updated upstream
+=======
+    public GameObject spellPrefab;
+    public Transform spawnPt;
+    private float x_component;
+    private float y_component;
+    private float prev_x = 0.0f;
+    private float prev_y = 0.0f;
+>>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
 
+=======
+        x_component = 0.0f;
+        y_component = 0.0f;
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
     void Update()
     {
         var input = Game.Input.Controls;
+<<<<<<< Updated upstream
         float x_movement = 0.0f;
         float y_movement = 0.0f;
         
         x_movement = input.AimBookX.ReadValue<float>();
         y_movement = input.AimBookY.ReadValue<float>();
+=======
+
+        x_component = input.AimBookX.ReadValue<float>();
+        y_component = input.AimBookY.ReadValue<float>();
+>>>>>>> Stashed changes
 
         if((input.AimBookRight.ReadValue<float>() != 0) && (input.AimBookLeft.ReadValue<float>() == 0))
         {
-            x_movement = input.AimBookRight.ReadValue<float>();
+            x_component = input.AimBookRight.ReadValue<float>();
         }
         else if((input.AimBookLeft.ReadValue<float>() != 0) && (input.AimBookRight.ReadValue<float>() == 0))
         {
-            x_movement = 0 - input.AimBookLeft.ReadValue<float>();
+            x_component = 0 - input.AimBookLeft.ReadValue<float>();
         }
 
         if((input.AimBookUp.ReadValue<float>() != 0) && (input.AimBookDown.ReadValue<float>() == 0))
         {
-            y_movement = input.AimBookUp.ReadValue<float>();
+            y_component = input.AimBookUp.ReadValue<float>();
         }
         else if((input.AimBookDown.ReadValue<float>() != 0) && (input.AimBookUp.ReadValue<float>() == 0))
         {
-            y_movement = 0 - input.AimBookDown.ReadValue<float>();
+            y_component = 0 - input.AimBookDown.ReadValue<float>();
+        }
+        if ((x_component != 0) || (y_component != 0))
+        {
+            prev_x = x_component;
+            prev_y = y_component;
         }
 
-        float angle = Mathf.Atan2(x_movement, y_movement) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(prev_x, prev_y) * Mathf.Rad2Deg;
 
+<<<<<<< Updated upstream
         if((x_movement != 0) || (y_movement != 0))
+=======
+        if ((x_component != 0) || (y_component != 0))
+>>>>>>> Stashed changes
         {
             transform.rotation = Quaternion.Euler(0, 0, 360-angle);
         }
