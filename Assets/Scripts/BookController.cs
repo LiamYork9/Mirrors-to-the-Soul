5f0f66fe0a9424b54932ea38bdda7b0bdf140ff4
Lily <ndd232@gmail.com>
2025-04-12 10:36:54 -0500

    Update again

--- Assets/Scripts/BookController.cs
+++ Assets/Scripts/BookController.cs
@@ -9,24 +9,24 @@ public class BookController : MonoBehaviour
     public float dead_zone;
     public GameObject spellPrefab;
     public Transform spawnPt;
+    private float x_component;
+    private float y_component;
+    private float prev_x = 0.0f;
+    private float prev_y = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        x_component = 0.0f;
-        y_component = 0.0f;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         var input = Game.Input.Controls;
-        float x_movement = 0.0f;
-        float y_movement = 0.0f;
 
-
-        x_movement = input.AimBookX.ReadValue<float>();
-        y_movement = input.AimBookY.ReadValue<float>();
+        x_component = input.AimBookX.ReadValue<float>();
+        y_component = input.AimBookY.ReadValue<float>();
 
         if ((input.AimBookRight.ReadValue<float>() != 0) && (input.AimBookLeft.ReadValue<float>() == 0))
         {
@@ -53,7 +53,7 @@ public class BookController : MonoBehaviour
 
         float angle = Mathf.Atan2(prev_x, prev_y) * Mathf.Rad2Deg;
 
-        if ((x_movement != 0) || (y_movement != 0))
+        if ((x_component != 0) || (y_component != 0))
         {
             transform.rotation = Quaternion.Euler(0, 0, 360 - angle);
         }
