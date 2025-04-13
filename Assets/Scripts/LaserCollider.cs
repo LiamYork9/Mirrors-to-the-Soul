9ff6a4de207607e18a6636e8eda19c5027f0fa88
Liam York <yorkliam08@gmail.com>
2025-04-13 00:07:43 -0500

    Fully working level 1

--- Assets/Scripts/LaserCollider.cs
+++ Assets/Scripts/LaserCollider.cs
@@ -8,7 +8,7 @@ public class LaserCollider : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        player_health = FindAnyObjectByType<PlayerHealth>();
     }
 
     // Update is called once per frame
